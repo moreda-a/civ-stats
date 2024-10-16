e5f8dddc4f058338fc806cbeb89bc01319b52ab2
Mohammadreza Daneshvaramoli <dos.moreda@yahoo.com>
2024-10-15 22:37:35 -0400

    from zero

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from app import create_app
+from flask import Flask
 
-app = create_app()
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello World 2!"
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
